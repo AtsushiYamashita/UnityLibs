223303b873da055b05fee119ee114b4b86e6142d
Atsushi_Yamashita <yays505@gmail.com>
2018-02-24 17:04:25 +0900

    Add Logger, NULL, Curry, Json, Array and Dictionary

---
+++ Assets/Statics/Logger.cs
@@ -0,0 +1,53 @@
+﻿#pragma warning disable 0414
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BasicExtends {
+
+    public class Logger {
+        private static Logger mInstance = new Logger();
+        private static Dictionary<string, string> mPlaceHolder
+            = new Dictionary<string, string>();
+        private Logger () { }
+
+        private static Logger LogPrint ( string str ) {
+            var stack = new System.Diagnostics.StackFrame(2);
+            var stack_arr = StArr.To(
+                stack.GetFileName(),
+                stack.GetFileLineNumber(),
+                stack.GetType().Name,
+                stack.GetMethod().Name
+                );
+            var now = DateTime.Now;
+            var date = StArr.To(
+                now.Minute,
+                now.Second,
+                now.Millisecond);
+            var dic = new Dictionary<string, string> {
+            { "msg", str },
+            { "date", date.Stringify("(","/",")")},
+            { "stack", stack_arr.Stringify("(","/",")")}
+        };
+            Debug.Log(dic.ToJson());
+            return mInstance;
+        }
+
+        public static Logger Log ( string str ) {
+            LogPrint(str);
+            return mInstance;
+        }
+        public static Logger Log ( string str, params object [] args ) {
+            LogPrint(string.Format(str, args));
+            return mInstance;
+        }
+
+        public Logger LogSend ( string to ) {
+            if (to == null) { return mInstance; }
+            if (to.Length < 1) { return mInstance; }
+
+            return mInstance;
+        }
+    }
+}
---
+++ Assets/Statics/StDic/StDicTest.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BasicExtends {
+
+    public class StDicTest: TestComponent<StDicTest> {
+        public string StringifyTest1 () {
+            var dict = new StringDict() { {"test","a" } };
+            var str = dict.Stringify();
+            if (str != "{test:a}") { return "文字列への変換に失敗しています　" + str; }
+            return "";
+        }
+        public string StringifyTest2 () {
+            var dict = new StringDict() { { "test", "a" },{ "t","b" } };
+            var str = dict.Stringify();
+            if (str != "{test:a,t:b}") { return "文字列への変換に失敗しています　" + str; }
+            return "";
+        }
+        public string Test () {
+            return "false";
+        }
+    }
+}
