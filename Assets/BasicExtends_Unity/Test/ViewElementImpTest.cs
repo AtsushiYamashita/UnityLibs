e665f46c543ab753c29b2559203136d70af0f6a5
gaudi2\tsumiki <asdsad@tsumikiseisaku.com>
2018-03-08 17:43:00 +0900

    Add file.

---
+++ Assets/BasicExtends_Unity/Test/ViewElementImpTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewElementImpTest : ScrollViewElement
+{
+    public void Start()
+    {
+        
+    }
+
+    public override void SetData(object obj)
+    {
+        mData = obj;
+    }
+}
