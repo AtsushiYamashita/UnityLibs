cc1aeab29528ffdcc13e45423eb2cb7fcd2b86e3
AtsushiYamashita <yays505@gmail.com>
2018-03-29 16:00:55 +0900

    Add files

---
+++ Assets/BasicExtends_Unity/Test/TransformRecorderTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using BasicExtends;
+
+public class TransformRecorderTest:  TestComponentMulti{
+
+    public void FailTest(string name, Action<string,string> result ) {
+        result.Invoke(name, Fail());
+    }
+
+}
