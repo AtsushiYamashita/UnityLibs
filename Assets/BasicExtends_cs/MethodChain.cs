4c1a4db3abc958f4c68e22f7629671153386c4f4
AtsushiYamashita <yays505@gmail.com>
2018-04-03 13:39:34 +0900

    Add files and place holder

---
+++ Assets/BasicExtends_cs/MethodChain.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicExtends {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class MethodChain {
+
+
+    }
+}
---
+++ Assets/BasicExtends_cs/Test/MethodChainTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BasicExtends;
+
+public class MethodChainTest : TestComponent {
+    public string FailTest (string name) {
+        return Fail();
+    }
+
+}
