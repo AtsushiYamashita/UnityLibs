21525607d35565260687310796f9b31897f061a3
AtsushiYamashita <yays505@gmail.com>
2018-04-13 11:22:08 +0900

    Update static array test

--- Assets/Statics/Test/StArrTest.cs
+++ Assets/Statics/Test/StArrTest.cs
@@ -20,30 +20,39 @@ public class StArrTest: TestComponent {
         if (ar3 == null) { return "型変換に失敗しています"; }
         return "";
     }
+
     public string StringifyTestHST0 () {
-        var arr = StArr.To("");
+        var arr = StArr.To();
         var str = arr.StringifyBasic(",");
-        if (str != h + t) { return "文字列化に失敗 " + str; }
-        return "";
+        if (str != h + t) { return Fail("文字列化に失敗 " + str); }
+        return Pass();
     }
+
     public string StringifyTestHST1 () {
         var arr = StArr.To("a");
         var str = arr.StringifyBasic(",");
-        if (str != h + "a" + t) { return "文字列化に失敗 " + str; }
-        return "";
+        if (str != h + "\"a\""+ t) { return Fail("文字列化に失敗 " + str); }
+        return Pass();
+    }
+
+    public string StringifyTestHST1_2 () {
+        var arr = StArr.To("a","b");
+        var str = arr.StringifyBasic(",");
+        if (str != h + "\"a\",\"b\"" + t) { return Fail("文字列化に失敗 " + str); }
+        return Pass();
     }
 
     public string StringifyTestHST2 () {
         var arr = StArr.To("a",1);
-        var str = arr.StringifyBasic(", ");
-        if (str != h + "a, 1" + t) { return "文字列化に失敗 " + str; }
-        return "";
+        var str = arr.StringifyBasic(",");
+        if (str != h + "\"a\",1" + t) { return Fail("文字列化に失敗 " + str); }
+        return Pass();
     }
 
     public string StringifyTest () {
-        var arr = StArr.To("a", 1);
-        var str = arr.Stringify(", ");
-        if (str != "a, 1" ) { return "文字列化に失敗 " + str; }
-        return "";
+        var arr = StArr.To("a", 1,"b");
+        var str = arr.Stringify(",");
+        if (str != "\"a\",1,\"b\"") { return Fail("文字列化に失敗 " + str); }
+        return Pass();
     }
 }
