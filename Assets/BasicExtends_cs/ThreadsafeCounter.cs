416ccf41ca7fe5a4f3f2d5c5bd0c334240a0a577
AtsushiYamashita <yays505@gmail.com>
2018-04-18 19:29:00 +0900

    Split network interface

---
+++ Assets/BasicExtends_cs/ThreadsafeCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BasicExtends {
+    public class ThreadsafeCounter {
+        private int mCounter = 0;
+        private System.Object mLock = new System.Object();
+        public ThreadsafeCounter Increment () {
+            lock (mLock) {
+                mCounter++;
+            }
+            return this;
+        }
+        public int Get () {
+            return mCounter;
+        }
+    }
+}
