b6413d526903505099219ead9dcac0ac4c92e06f
AtsushiYamashita <yays505@gmail.com>
2018-04-28 19:43:36 +0900

    Split Find extend files

---
+++ Assets/BasicExtends/Unity/Find/FindStore.cs
@@ -0,0 +1,90 @@
+﻿namespace BasicExtends {
+    using System.Linq;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// sarchKey = nameかtagを使って、
+    /// 登録したり
+    /// </summary>
+    public class FindStore: Singleton<FindStore> {
+        private Dictionary<string, FindableData> mDatas = new Dictionary<string, FindableData>();
+        private Dictionary<string, List<FindableData>> mTagedDatas = new Dictionary<string, List<FindableData>>();
+
+        private FindStore () { } // seal
+
+        /// <summary>
+        /// Clear all stored data.
+        /// You will use game data reset.
+        /// </summary>
+        public void Clear () {
+            mDatas = new Dictionary<string, FindableData>();
+            mTagedDatas = new Dictionary<string, List<FindableData>>();
+        }
+
+        public int CountbyTag ( string str ) {
+            if (mTagedDatas.ContainsKey(str) == false) { return 0; }
+            return mTagedDatas [str].Count;
+        }
+
+        public void AddByKey ( FindableData data ) {
+            var key = data.SarchKey;
+            if (key == string.Empty) { return; }
+
+            var keyContained = mDatas.ContainsKey(key);
+            if (keyContained) {
+                string str = string.Format("The key:{0} is used by {1},{2}"
+                    , key, mDatas [key].Object, data.Object);
+                throw new System.Exception(str);
+            }
+            mDatas.TrySet(key, data);
+        }
+
+        public void AddByTag ( FindableData data ) {
+            foreach (var tag in data.Tags) {
+                if (!mTagedDatas.ContainsKey(tag)) { mTagedDatas.Add(tag, new List<FindableData>()); }
+                var contain = mTagedDatas [tag].Where(e => { return e.Object == data.Object; });
+                if (contain != null) { return; }
+                mTagedDatas [tag].Add(data);
+            }
+        }
+
+        /// <summary>
+        /// Get named object.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>When that named object is nothing then return null</returns>
+        public GameObject GetNamed ( string name ) {
+            FindableData data = null;
+            var exist = mDatas.TryGetValue(name, out data);
+            if (!exist) { return null; }
+            if (data == null) { return null; }
+            if (data.Object == null) {
+                mDatas.Remove(name);
+                return null;
+            }
+            return data.Object;
+        }
+
+        /// <summary>
+        /// Get tagged object.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public List<GameObject> GetTagged ( string tag ) {
+            List<FindableData> datas = null;
+            var exist = mTagedDatas.TryGetValue(tag, out datas);
+            if (!exist) { return null; }
+            if (datas == null) { return null; }
+
+            var dat2 = datas.Where(e =>
+            {
+                if (e == null) { return false; }
+                if (e.Object == null) { return false; }
+                return true;
+            }).ToList();
+            mTagedDatas [tag] = dat2;
+            return dat2.Select(e => { return e.Object; }).ToList();
+        }
+    }
+}
---
+++ Assets/BasicExtends/Unity/Find/FindableData.cs
@@ -0,0 +1,60 @@
+﻿namespace BasicExtends {
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// This data include for you can find this object.
+    /// </summary>
+    [System.Serializable]
+    public class FindableData {
+        private GameObject mObject = null;
+        public GameObject Object
+        {
+            get { return mObject; }
+            set { this.mObject = value; }
+        }
+
+        [SerializeField]
+        private string mSarchKey = string.Empty;
+        public string SarchKey
+        {
+            get { return mSarchKey; }
+            set { this.mSarchKey = value; }
+        }
+
+        [SerializeField]
+        private List<string> mTags = new List<string>();
+        public void SetTags ( params string [] args ) {
+            mTags = new List<string>();
+            AddTags(args);
+        }
+        public void AddTags ( params string [] args ) {
+            mTags.AddRange(args);
+            FindStore.Instance.AddByTag(this);
+        }
+
+        public List<string> Tags
+        {
+            get { return mTags; }
+        }
+
+        [SerializeField]
+        private int mIndex = -1;
+        public int Index
+        {
+            get { return mIndex; }
+            set { this.mIndex = value; }
+        }
+
+        public override string ToString () {
+            string ret;
+            ret = string.Format("GameObject:{0}\n", Object.GetObjectPath());
+            for (int i = 0; i < mTags.Count; i++) {
+                ret += string.Format("tag{0}:{1}\n", i, mTags [i]);
+            }
+            ret += string.Format("index:{0}\n", mIndex);
+            return ret;
+        }
+    }
+
+}
