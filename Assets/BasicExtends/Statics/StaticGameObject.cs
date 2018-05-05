6cdddea78659423b0d1e60302cb9b22f1ab73d90
Atsushi_Yamashita <yays505@gmail.com>
2018-05-06 06:58:46 +0900

    #66 Add comment at Static game object

--- Assets/BasicExtends/Statics/StaticGameObject.cs
+++ Assets/BasicExtends/Statics/StaticGameObject.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using System;
 
 namespace BasicExtends {
     public static class StaticGameObject {
+
+        /// <summary>
+        /// UnityでGameObjectの子供として扱われているオブジェクトを、
+        /// リスト形式で一度に取得できます
+        /// 通常のChildrenのリストから非アクティブを除去しているため低速です。
+        /// 
+        /// Usage
+        /// var childrenList = gameObject.Children();
+        /// foreach(var child in childrenList){ .... }
+        /// </summary>
+        /// <param name="obj">子供を取得したいオブジェクト</param>
+        /// <returns>取得された子供</returns>
+        public static List<GameObject> ActiveChildren ( this GameObject obj ) {
+            return Children(obj).Where(e=>e.activeSelf).ToList();
+        }
 
         /// <summary>
         /// UnityでGameObjectの子供として扱われているオブジェクトを、
@@ -15,15 +31,24 @@ namespace BasicExtends {
         /// </summary>
         /// <param name="obj">子供を取得したいオブジェクト</param>
         /// <returns>取得された子供</returns>
-        public static List<GameObject>  Children ( this GameObject obj ) {
+        public static List<GameObject> Children ( this GameObject obj ) {
             var children = new List<GameObject>();
             var tr = obj.transform;
             for (int i = 0; i < tr.childCount; i++) {
                 var ch = tr.GetChild(i).gameObject;
-                if (ch.activeSelf == false) { continue; }
                 children.Add(ch);
             }
             return children;
+        }
+
+        /// <summary>
+        /// 指定したリストの中のゲームオブジェクトをすべてActive-falseにする。
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static List<GameObject> AllSleep ( this List<GameObject> target ) {
+            foreach (var c in target) { c.SetActive(false); }
+            return target;
         }
 
         public static GameObject FindBrother ( this GameObject obj, string name ) {
@@ -54,7 +79,7 @@ namespace BasicExtends {
             return obj.gameObject.GetInstanceID();
         }
 
-        public static string GetObjectPath (this GameObject obj) {
+        public static string GetObjectPath ( this GameObject obj ) {
             string ret = obj.name;
             Transform parent = obj.transform.parent;
             while (parent != obj.transform.root) {
