1ca35d9d328db64f7204715ee21bde94476ed9ce
gaudi2\tsumiki <asdsad@tsumikiseisaku.com>
2018-05-16 21:18:08 +0900

    Update for support msg new style

--- Assets/BasicExtends/Scenario/Unit/SenarioIterator.cs
+++ Assets/BasicExtends/Scenario/Unit/SenarioIterator.cs
@@ -1,14 +1,17 @@
-﻿namespace BasicExtends.Scenario {
+﻿namespace BasicExtends.Scenario
+{
     using UnityEngine;
     using UnityEngine.Assertions;
     using UnityEngine.Events;
     using BasicExtends;
 
-    public interface ICanSkip {
-        void Skip ();
+    public interface ICanSkip
+    {
+        void Skip();
     }
 
-    public class SenarioIterator<T>: MonoBehaviour, ICanSkip, IFactoryReceiver where T : MonoBehaviour {
+    public class SenarioIterator<T> : MonoBehaviour, ICanSkip, IFactoryReceiver where T : MonoBehaviour
+    {
 
         // 0番をActiveなレイヤーとした疑似スタック構造を保持する
         [SerializeField]
@@ -26,16 +29,21 @@
         protected string TypeName { get; set; }
         protected IFactory mFactory = null;
 
-        public void InstanceSet ( T t ) {
+        public void InstanceSet(T t)
+        {
             PushHead(t);
         }
 
-        private void ChangeMessage (string f, string t) {
+        private void ChangeMessage(string f, string t)
+        {
             var str = string.Format("Change {0} -> {1}", f, t);
-            Msg.Gen().To("Debug").Act("Log").Set("msg",str).Push();
+            Msg.Gen().Set(Msg.TO, "Debug")
+                .Set(Msg.ACT, "Log")
+                .Set("msg", str).Push();
         }
 
-        public void PushHead ( T unit ) {
+        public void PushHead(T unit)
+        {
             mChangeStart.Invoke();
             Assert.IsNotNull(unit);
             var old = mStack.Head.IsNull() ? "---" : mStack.Head.name;
@@ -48,37 +56,44 @@
             mStack.Push(unit);
         }
 
-        public void Remove () {
+        public void Remove()
+        {
             mChangeStart.Invoke();
 
             var t = mStack.Pop();
-            Assert.IsTrue(t.Key,"取得に失敗しました");
+            Assert.IsTrue(t.Key, "取得に失敗しました");
             var obj = t.Value.gameObject;
             ChangeMessage(obj.name, mStack.Head.name);
 
-            if (mStack.Head == null) {
+            if (mStack.Head == null)
+            {
                 return;
             }
             mStack.Head.gameObject.SetActive(true);
             mChangeEnd.Invoke();
         }
 
-        public void Skip () {
-            while (mStack.Head != null && mStack.Count > 0) {
+        public void Skip()
+        {
+            while (mStack.Head != null && mStack.Count > 0)
+            {
                 var t = mStack.Pop();
                 var itr = t as ICanSkip;
                 if (itr.IsNotNull()) { itr.Skip(); }
                 Assert.IsTrue(t.Key, "取得に失敗しました");
                 var obj = t.Value.gameObject;
             }
-            Msg.Gen().To("Debug").Act("Log").Set("msg",name + " is skiped.").Push();
+            Msg.Gen().Set(Msg.TO, "Debug")
+                .Set(Msg.ACT, "Log")
+                .Set("msg", name + " is skiped.").Push();
             gameObject.SetActive(false);
             mUnitSkip.Invoke();
         }
 
-        protected virtual void StartProcess () { }
+        protected virtual void StartProcess() { }
 
-        private void Start () {
+        private void Start()
+        {
             mFactory = mFactory ?? new TokenbaseParser<T>();
             mFactory.SetTarget(this);
             AssignChildren();
@@ -87,18 +102,22 @@
             {
                 if (msg.Unmatch("To", gameObject.name)) { return; }
                 if (msg.Unmatch("as", GetType().Name)) { return; }
-                if (msg.Match("act", "PushHead")) {
+                if (msg.Match("act", "PushHead"))
+                {
                     PushHead(msg.TryObjectGet<T>());
                     return;
                 }
-                if (msg.Match("act", "remove")) {
+                if (msg.Match("act", "remove"))
+                {
                     Remove();
                     return;
                 }
-                if (msg.Match("act", "Skip")) {
+                if (msg.Match("act", "Skip"))
+                {
                     throw new System.NotImplementedException();
                 }
-                if (msg.Match("act", "SetScript")) {
+                if (msg.Match("act", "SetScript"))
+                {
                     mFactory.SetScript(msg.TryGet("script"));
                     return;
                 }
@@ -110,9 +129,11 @@
         /// Activeな子が見つかったら
         /// そこまでスキップするという処理
         /// </summary>
-        private void SkipOnStart (GameObject obj) {
+        private void SkipOnStart(GameObject obj)
+        {
             if (obj.gameObject.activeSelf) { return; }
-            while ( mStack.Count > 0) {
+            while (mStack.Count > 0)
+            {
                 var target = mStack.Pop();
                 if (target.Check == false) { continue; }
                 var skip = target.Value as ICanSkip;
@@ -125,24 +146,28 @@
         /// Tに該当するコンポネントを持ったものを抽出し、
         /// 上から順に実行する
         /// </summary>
-        private void AssignChildren () {
+        private void AssignChildren()
+        {
             var children = gameObject.Children();
             children.Reverse();
             var active = 0;
-            foreach(var c in children) {
+            foreach (var c in children)
+            {
                 var t = c.GetComponent<T>();
-                if(t == null) { continue; }
+                if (t == null) { continue; }
                 SkipOnStart(t.gameObject);
                 mStack.Push(t);
                 active += t.gameObject.activeSelf ? 1 : 0;
             }
 
-            if(mStack.Count <= 0) {
+            if (mStack.Count <= 0)
+            {
                 throw new System.Exception("イテレーションの対象がありません");
             }
 
             // アクティブな進行対象がなければ、一番上のオブジェクトを有効化する。
-            if (active == 0 ) {
+            if (active == 0)
+            {
                 PushHead(mStack.Pop().Value);
                 active++;
             }
@@ -152,7 +177,8 @@
         /// <summary>
         /// seal
         /// </summary>
-        void IFactoryReceiver.InstanceSet<T1> ( T1 t ) {
+        void IFactoryReceiver.InstanceSet<T1>(T1 t)
+        {
             throw new System.NotImplementedException();
         }
     }
