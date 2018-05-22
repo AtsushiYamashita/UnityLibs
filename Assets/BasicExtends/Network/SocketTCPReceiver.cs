326bd3f06e74df6b9bdf3f13bfe199e89d53bca5
AtsushiYamashita <yays505@gmail.com>
2018-05-23 07:04:11 +0900

    Add Socket TCP/UDP

---
+++ Assets/BasicExtends/Network/SocketTCPReceiver.cs
@@ -0,0 +1,48 @@
+﻿namespace BasicExtends {
+    using UnityEngine;
+    using System.Net;
+    using System.Net.Sockets;
+
+
+    public class SocketTCPReceiver: MonoBehaviour {
+        [SerializeField]
+        private int mReceivePort = 0;
+        private Socket mConnectSocket = null;
+
+        private ThreadState MakeConnection (object obj) {
+            var listen = (Socket) obj;
+            var isConnect = listen.Poll(0, SelectMode.SelectRead);
+            if (isConnect == false) {
+                System.Threading.Thread.Sleep(5);
+                return ThreadState.Continue;
+            }
+            mConnectSocket = listen.Accept();
+            return ThreadState.End;
+        }
+
+        private ThreadState Receive(object obj ) {
+            if (mConnectSocket == null) {
+                System.Threading.Thread.Sleep(10);
+                return ThreadState.Continue;
+            }
+            byte [] buffer = new byte [2500];
+            int size = mConnectSocket
+                .Receive(buffer, buffer.Length, SocketFlags.None);
+            if (size < 1) { return ThreadState.Continue; }
+            var ret = Serializer.Deserialize(ByteList.Gen().Add(buffer));
+            if (ret.Key == false) { return ThreadState.Continue; }
+            return ThreadState.Continue;
+        }
+
+        private void Setup () {
+            var listen = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            listen.Bind(new IPEndPoint(IPAddress.Any, mReceivePort));
+            listen.Listen(1);
+
+            ThreadManager.Get().Work(listen, MakeConnection );
+            ThreadManager.Get().Work(null, Receive);
+        }
+    }
+
+}
---
+++ Assets/BasicExtends/Network/SocketTCPSender.cs
@@ -0,0 +1,82 @@
+﻿namespace BasicExtends {
+    using System.Net;
+    using System.Net.Sockets;
+    using UnityEngine;
+
+    public class SocketTCPSender : MonoBehaviour {
+
+        [SerializeField]
+        private int mSendToPort = 0;
+
+        [SerializeField]
+        private string mSendToIp = "";
+
+        private Socket mSocket = null;
+        private ThreadsafeCounter mSendId = new ThreadsafeCounter();
+        bool mIsSetuped = false;
+        private SafeAccessList<byte []> mMsgList = new SafeAccessList<byte []>();
+
+        private static string GetOwnIP () {
+            try {
+                //ホスト名を取得
+                string hostname = Dns.GetHostName();
+
+                //ホスト名からIPアドレスを取得
+                IPAddress [] addr_arr = Dns.GetHostAddresses(hostname);
+
+                foreach (IPAddress addr in addr_arr) {
+                    string addr_str = addr.ToString();
+                    bool isIPv4 = addr_str.IndexOf(".") > 0;
+                    bool notLocalhost = addr_str.StartsWith("127.") == false;
+                    if (isIPv4 && notLocalhost) { return addr_str; }
+                }
+            } catch (System.Exception e) {
+                Msg.Gen().Set(Msg.TO, "Debug")
+                    .Set(Msg.ACT, "log")
+                    .Set(Msg.MSG, e.ToString()).Pool();
+            }
+            return "";
+        }
+
+        private void Setup () {
+            var recv_ip = GetOwnIP();
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.NoDelay = true;
+            socket.SendBufferSize = 0;
+            ThreadManager.Get().Work(null, ( obj ) =>
+            {
+                IPAddress recv = IPAddress.Any;
+                try {
+                    socket.Connect(mSendToIp, mSendToPort);
+                } catch {
+                    return ThreadState.Continue;
+                }
+                mIsSetuped = true;
+                return ThreadState.End;
+            });
+
+            ThreadManager.Get().Work(null, ( obj ) =>
+            {
+                byte [] buffer = mMsgList.Pop();
+                if (buffer == null || buffer.Length < 1) {
+                    System.Threading.Thread.Sleep(NetworkUnit.INTERVAL);
+                    return ThreadState.Continue ;
+                }
+                socket.Send(buffer, buffer.Length,SocketFlags.None);
+                return ThreadState.Continue;
+            });
+
+        }
+
+        private void SendStack ( Msg message ) {
+            message = message
+                .Set("Id", "" + mSendId.Get());
+            mSendId.Increment();
+            if (mIsSetuped == false) { return; }
+            mMsgList.Add(Serializer.Serialize(message).ToArray());
+        }
+
+    }
+
+
+}
