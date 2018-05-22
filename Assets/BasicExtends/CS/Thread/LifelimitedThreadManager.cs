2558ad47df4fae464823cac57f1060f2b51b38ba
AtsushiYamashita <yays505@gmail.com>
2018-05-23 07:04:39 +0900

    Replace life thread

--- Assets/BasicExtends/CS/Thread/LifelimitedThreadManager.cs
+++ Assets/BasicExtends/CS/Thread/LifelimitedThreadManager.cs
@@ -1,5 +1,4 @@
-﻿namespace BasicExtends
-{
+﻿namespace BasicExtends {
     using System;
     using System.Threading;
     using System.Collections.Generic;
@@ -8,10 +7,8 @@
 
     public enum ThreadState { Continue, End }
 
-    public class LifelimitedThread
-    {
-        internal LifelimitedThread(int id)
-        {
+    public class LifeThread {
+        internal LifeThread ( int id ) {
             ID = id;
         }
 
@@ -19,63 +16,57 @@
         private Thread mThread = null;
         public int ID { private get; set; }
 
-        public void Work(Func<object, ThreadState> action, object obj)
-        {
+        public void Work ( object obj, Func<object, ThreadState> action ) {
             mThread = new Thread(() =>
             {
-                while (action(obj) == ThreadState.Continue) { }
+                while (action(obj) == ThreadState.Continue) { Thread.Sleep(1); }
                 mThread.Abort();
                 mThread = null;
             });
         }
     }
 
-    public class ThreadManager : Singleton<ThreadManager>
-    {
-        private Dictionary<int, LifelimitedThread> mHolding = new Dictionary<int, LifelimitedThread>();
-        private StackLikeList<LifelimitedThread> mUnWorking = new StackLikeList<LifelimitedThread>();
+    public class ThreadManager: Singleton<ThreadManager> {
+        private Dictionary<int, LifeThread> mHolding = new Dictionary<int, LifeThread>();
+        private StackLikeList<LifeThread> mUnWorking = new StackLikeList<LifeThread>();
         private object mLock = new object();
         public int Holding { get { return mHolding.Count; } }
         public long ThreadDead { internal set; get; }
         internal bool Enable = false;
 
-        public void PoolFill(int num)
-        {
-            for (int i = 0; i < num; i++)
-            {
-                lock (mLock)
-                {
+        public void PoolFill ( int num ) {
+            for (int i = 0; i < num; i++) {
+                lock (mLock) {
                     var h = Holding;
-                    var t = new LifelimitedThread(h);
+                    var t = new LifeThread(h);
                     mHolding.TrySet(h, t);
                 }
             }
         }
 
-        public static LifelimitedThread Get()
-        {
+        public static LifeThread Get () {
             Assert.IsTrue(Instance.Enable);
-            var ret = Instance.mUnWorking.Pop();
+            CheckedRet<LifeThread> ret = null;
+            lock (Instance.mLock) {
+                ret = Instance.mUnWorking.Pop();
+            }
             if (ret.Key == false) { Instance.PoolFill(2); }
             return ret.Value;
         }
     }
 
-    public class LifelimitedThreadManager : MonoBehaviour
-    {
-        private void DeadTimeUpdate()
-        {
-            ThreadManager.Instance.ThreadDead = DateTime.Now.Ticks + 100 * 1000;
+    public class LifelimitedThreadManager: MonoBehaviour {
+        private void DeadTimeUpdate () {
+            ThreadManager.Instance.ThreadDead
+                = DateTime.Now.Ticks + 1000 * 1000;
         }
 
-        private void Start()
-        {
+        private void Start () {
             ThreadManager.Instance.Enable = true;
             DeadTimeUpdate();
         }
 
-        private void Update()
-        {
+        private void Update () {
             DeadTimeUpdate();
         }
     }
