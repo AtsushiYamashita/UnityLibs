ea3c9a0d1be6311d4f3ee40f400324a697f8c0e5
AtsushiYamashita <yays505@gmail.com>
2018-05-24 15:32:59 +0900

    Update static parser in string

--- Assets/BasicExtends/Statics/StString.cs
+++ Assets/BasicExtends/Statics/StString.cs
@@ -16,6 +16,20 @@
         public static string ToUTF8String ( this ByteList bytes ) {
             return Encoding.UTF8.GetString(bytes.ToArray());
         }
+
+        /// <summary>
+        /// 文字列を簡易に整数へ変換する
+        /// </summary>
+        public static int ParseInt(this string str ) {
+            return int.Parse(str);
+        }
+
+        /// <summary>
+        /// 文字列を簡易に浮動小数点数へ変換する
+        /// </summary>
+        public static float ParseFloat ( this string str ) {
+            return float.Parse(str);
+        }
     }
 }
 
