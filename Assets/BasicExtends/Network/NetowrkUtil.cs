2c198337d59a8ac458e1ec8053a27404b9769631
AtsushiYamashita <yays505@gmail.com>
2018-05-24 15:31:57 +0900

    Update network manager and util

---
+++ Assets/BasicExtends/Network/NetowrkUtil.cs
@@ -0,0 +1,38 @@
+﻿namespace BasicExtends {
+    using System.Net;
+
+    /// <summary>
+    /// ネットワークに関係する汎用機能を集めたクラス
+    /// </summary>
+    public static class NetowrkUtil {
+
+        /// <summary>
+        /// 自分のローカルIPを取得する。
+        /// 値が取れない場合はEmptyが返る
+        /// </summary>
+        public static string GetOwnIP () {
+            System.Exception exception = new System.Exception();
+            try {
+                //ホスト名を取得
+                string hostname = Dns.GetHostName();
+
+                //ホスト名からIPアドレスを取得
+                IPAddress [] addr_arr = Dns.GetHostAddresses(hostname);
+
+                foreach (IPAddress addr in addr_arr) {
+                    string addr_str = addr.ToString();
+                    bool isIPv4 = addr_str.IndexOf(".") > 0;
+                    bool notLocalLoopback = addr_str.StartsWith("127.") == false;
+                    if (isIPv4 && notLocalLoopback) { return addr_str; }
+                }
+            } catch (System.Exception e) {
+                exception = e;
+            }
+            Msg.Gen().Set(Msg.TO, "Debug")
+                .Set(Msg.ACT, "log")
+                .Set(Msg.MSG, exception.ToString()).Pool();
+            return string.Empty;
+        }
+    }
+
+}
--- Assets/BasicExtends/Network/NetworkManager.cs
+++ Assets/BasicExtends/Network/NetworkManager.cs
@@ -1,47 +1,53 @@
-﻿using UnityEngine;
-using BasicExtends;
+﻿namespace BasicExtends {
+    using UnityEngine;
+    using System.Collections.Generic;
 
-public class NetworkManager: MonoBehaviour {
+    public class NetworkManager: MonoBehaviour {
 
-    private void Start () {
-        MessengerSetup();
-        Serializer.SetDatatype(Serializer.SerialType.Binary2);
-        Msg.Gen().Set(Msg.TO, gameObject.name)
-            .Set(Msg.AS, "UdpReceiver")
-            .Set(Msg.ACT, "Start")
-            .Pool();
-        Msg.Gen().Set(Msg.TO, gameObject.name)
-            .Set(Msg.AS, "UdpSender")
-            .Set(Msg.ACT, "Setup")
-            .Pool();
-    }
+        private string mOwnIp = string.Empty;
 
-    private void MessengerSetup () {
-        Messenger.Assign(( Msg msg ) =>
-        {
-            if (msg.Unmatch(Msg.TO, "Manager")) { return; }
-            if (msg.Unmatch(Msg.AS, "NetworkManager")) { return; }
-            if (msg.Match("result", "Fail") ) {
-                FailEvent(msg);
-                return;
-            }
-            if (msg.Match("result", "Success") ) {
-                Debug.Log(msg.Stringify()); // for debug
-                return;
-            }
-            if (msg.ContainsKey("From")) {
-                Debug.Log(msg.Stringify());
-                return;
-            }
-        });
-    }
+        private void Start () {
+            MessengerSetup();
+            Serializer.SetDatatype(Serializer.SerialType.Binary2);
+            Msg.Gen().Set(Msg.TO, gameObject.name)
+                .Set(Msg.AS, "UdpReceiver")
+                .Set(Msg.ACT, "Start")
+                .Pool();
+            Msg.Gen().Set(Msg.TO, gameObject.name)
+                .Set(Msg.AS, "UdpSender")
+                .Set(Msg.ACT, "Setup")
+                .Pool();
+            mOwnIp = NetowrkUtil.GetOwnIP();
+        }
 
-    private void Update () {
-        Messenger.Flash();
-    }
+        private void MessengerSetup () {
+            Messenger.Assign(( Msg msg ) =>
+            {
+                if (msg.Unmatch(Msg.TO, "Manager")) { return; }
+                if (msg.Unmatch(Msg.AS, "NetworkManager")) { return; }
+                if (msg.Match("result", "Fail")) {
+                    FailEvent(msg);
+                    return;
+                }
+                if (msg.Match("result", "Success")) {
+                    Debug.Log(msg.Stringify()); // for debug
+                    return;
+                }
+                if (msg.ContainsKey("From")) {
+                    Debug.Log(msg.Stringify());
+                    return;
+                }
+            });
+        }
 
-    private void FailEvent ( Msg msg ) {
-        Debug.Log(msg.Stringify());
+        private void Update () {
+            Messenger.Flash();
+        }
+
+        private void FailEvent ( Msg msg ) {
+            Debug.Log(msg.Stringify());
+        }
+
     }
 
 }
