415ac9a8943864a67dc7408f2b601d6db4c717ed
AtsushiYamashita <yays505@gmail.com>
2018-06-05 16:59:15 +0900

    Add network files

---
+++ Assets/BasicExtends/Network/Test/NetworkTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BasicExtends;
+
+
+[RequireComponent(typeof(Renderer))]
+public class NetworkTest: MonoBehaviour {
+
+    private Renderer mOwnRenderer = null;
+
+    [SerializeField]
+    private KeyCode mEvent = KeyCode.D;
+
+    private void Start () {
+        mOwnRenderer = GetComponent<Renderer>();
+
+        Messenger.Assign(( msg ) =>
+        {
+            if (msg.Match("Network", "True")) { return; }
+            if (msg.Unmatch(Msg.TO, name)) { return; }
+            if (msg.Unmatch(Msg.AS, GetType().Name)) { return; }
+            if (msg.Match(Msg.ACT, "Switch")) {
+                Switch();
+                return;
+            }
+        });
+    }
+
+    private void Update () {
+        if (Input.GetKeyDown(mEvent) == false) { return; }
+        Debug.Log("send");
+        Msg.Gen().Set(Msg.TO, name)
+            .Set(Msg.AS, GetType().Name)
+            .Set(Msg.ACT, "Switch")
+            .Netwrok()
+            .Push();
+    }
+
+    private void Switch () {
+        mOwnRenderer.enabled = !mOwnRenderer.enabled;
+    }
+}
---
+++ Assets/BasicExtends/Network/Test/SocketUDPTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BasicExtends;
+
+public class SocketUDPTestScript : MonoBehaviour {
+
+    public SocketUDPReceiver mRec;
+    public SocketUDPSender mSend;
+
+	public void Setup () {
+        mRec.Setup();
+        mSend.Setup();
+    }
+
+    public void SendTest1 () {
+        mSend.SendStack(Msg.Gen()
+            .Set(Msg.TO, "Debug").Set(Msg.ACT, "log")
+            .Set(Msg.MSG, "=================="));
+    }
+
+}
